34722d549be60b2994ac4f2fd312dec296444953
jKreshnik <kreshnikj@paygilant.com>
2023-02-07 01:25:25 +0100

    remove leftover

--- src/main/java/com/kjusufi/net/calculator/TheNetCalculatorServiceApplication.java
+++ src/main/java/com/kjusufi/net/calculator/TheNetCalculatorServiceApplication.java
@@ -8,13 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TheNetCalculatorServiceApplication {
 
-	private static NetCalculatorService netCalculatorService;
-
-	@Autowired
-	public TheNetCalculatorServiceApplication(NetCalculatorService netCalculatorService) {
-		TheNetCalculatorServiceApplication.netCalculatorService = netCalculatorService;
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(TheNetCalculatorServiceApplication.class, args);
 	}
